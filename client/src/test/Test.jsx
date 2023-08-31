d54871ec3619bba8e71fc615ca66fe8f23fde8c4
HelloWorldDogs223 <yonghyeogi377@gmail.com>
2023-08-31 11:46:31 +0900

    TextEditor

---
+++ client/src/test/Test.jsx
@@ -0,0 +1,26 @@
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import "../../ckeditor5/build/ckeditor";
+import styled from "styled-components";
+
+const MyDiv = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+function Test() {
+  return (
+    <MyDiv>
+      <CKEditor
+        editor={ClassicEditor}
+        data="<p><span style='color:hsl(0, 0%, 0%);'>입력하세요</span></p>"
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          console.log(data);
+        }}
+      />
+    </MyDiv>
+  );
+}
+
+export default Test;
