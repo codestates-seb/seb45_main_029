f84229bb57f65706b54b1cdeb05d9044bf66b3f9
HelloWorldDogs223 <yonghyeogi377@gmail.com>
2023-09-04 09:42:09 +0900

    Main Screen Delete 제거

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,32 +1,28 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import "./App.css";
-import Main from './pages/Main';
-import MyPage from './pages/MyPage';
+import Main from "./pages/Main";
+import MyPage from "./pages/MyPage";
 // import BoardPage from './pages/BoardPage';
-import SignUp from './pages/SignUp';
+import SignUp from "./pages/SignUp";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import GlobalStyles from 'styled-components'
-
-import MyPageDelete from "./pages/MyPageDelete";
+import GlobalStyles from "styled-components";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
-      <GlobalStyles/>
+      <GlobalStyles />
       <BrowserRouter>
         <Routes>
-          <Route exact path='/' element={<Main />} />
-          <Route path='/mypage' element={<MyPage />} />
+          <Route exact path="/" element={<Main />} />
+          <Route path="/mypage" element={<MyPage />} />
           {/* <Route path='/boardpage' element={<BoardPage />} /> */}
-          <Route path='/signup' element={<SignUp />} />
-        
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </BrowserRouter>
-      <MyPageDelete />
-      <Footer/>
+      <Footer />
     </>
   );
 }
