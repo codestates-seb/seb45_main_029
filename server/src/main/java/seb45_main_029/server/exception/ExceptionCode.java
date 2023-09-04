6b34ad95154ddb12d6d881710fd1a07c56b33131
uudean <swj4545@naver.com>
2023-09-04 11:58:03 +0900

    update

---
+++ server/src/main/java/seb45_main_029/server/exception/ExceptionCode.java
@@ -0,0 +1,20 @@
+package seb45_main_029.server.exception;
+
+import lombok.Getter;
+
+public enum ExceptionCode {
+    // 각자 필요한 부분 작성
+    MEMBER_NOT_FOUND(404,"Member Not Found"),
+    VIDEO_NOT_FOUND(404,"Video Not Found");
+
+    @Getter
+    private int status;
+
+    @Getter
+    private String message;
+
+    ExceptionCode(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
