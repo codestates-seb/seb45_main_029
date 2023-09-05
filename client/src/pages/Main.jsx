1bf686e1d5147d43a75bf7cd4fa7cfdbdbcee351
HelloWorldDogs223 <yonghyeogi377@gmail.com>
2023-09-05 12:25:04 +0900

    Job List Added

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -1,15 +1,13 @@
 // eslint-disable-next-line no-unused-vars
-import Header from "../components/Header";
-
+import Header from '../components/Header';
 // eslint-disable-next-line no-unused-vars
 
-
 function Main() {
-    return <>
-        <Header />
-        <h1>home</h1>
-        
+  return (
+    <>
+      <Header />
+      <h1>home</h1>
     </>
-
+  );
 }
 export default Main;
