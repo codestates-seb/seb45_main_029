e15078d1873348faef9822d7ba8a095d8f2617f1
uudean <swj4545@naver.com>
2023-09-06 17:00:26 +0900

    Feat : 코드 수정

--- server/src/main/java/seb45_main_029/server/video/service/VideoService.java
+++ server/src/main/java/seb45_main_029/server/video/service/VideoService.java
@@ -4,17 +4,22 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import seb45_main_029.server.exception.BusinessLogicException;
 import seb45_main_029.server.exception.ExceptionCode;
+import seb45_main_029.server.security.auth.utils.AuthUserUtils;
 import seb45_main_029.server.user.entity.User;
 import seb45_main_029.server.user.repository.UserRepository;
+import seb45_main_029.server.user.service.UserService;
 import seb45_main_029.server.video.entity.Video;
 import seb45_main_029.server.video.repository.VideoRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @RequiredArgsConstructor
@@ -23,29 +28,35 @@ public class VideoService {
 
     private final VideoRepository videoRepository;
     private final UserRepository userRepository;
+    private final UserService userService;
 
     //            부위별 운동영상 리스트 조회
     @Transactional(readOnly = true)
-    public Page<Video> getPartVideos(int page, int size, String part) {
+    public Page<Video> getPartVideos(int page, int size, Video.BodyPart part) {
 //        부위와 동일한 태그를 가진 동영상을 가져옴
 
-        return videoRepository.findByCategory(PageRequest.of(page, size), part);
+        return videoRepository.findByBodyPart(PageRequest.of(page, size), part);
 
     }
 
 
     //        직업별 운동영상 리스트 조회
-//    @Transactional(readOnly = true)
+    @Transactional(readOnly = true)
     public Page<Video> getJobVideos(int page, int size) {
+        String loginUserEmail = getLoginUserPrincipal();
 
+        User findUser = userRepository.findByEmail(loginUserEmail).orElseThrow(() -> new BusinessLogicException(ExceptionCode.USER_NOT_FOUND));
+        String job = findUser.getJob();
 
         return videoRepository.findByTitleContaining(PageRequest.of(page, size), job);
 
     }
 
-    //    맞춤 운동 영상 조회
-//    @Transactional(readOnly = true)
+    //        맞춤 운동 영상 조회
+    @Transactional(readOnly = true)
     public Page<Video> getRecommendedVideos(int page, int size) {
+        User getLoginUser = userService.getLoginUser();
+        String userStatus = getLoginUser.getStatus();
 
 
         return videoRepository.findByTitleContaining(PageRequest.of(page, size), userStatus);
@@ -55,6 +66,7 @@ public class VideoService {
     //    인기 동영상 조회
     @Transactional(readOnly = true)
     public Page<Video> getPopularVideos(int page, int size) {
+
         return videoRepository.findAll(PageRequest.of(page, size, Sort.by("bookmarkCount").descending()));
     }
 
@@ -74,19 +86,41 @@ public class VideoService {
 
     public User bookmark(long videoId) {
 //        사용자가 북마크 버튼을 누르면 로그인한 정보를 기반으로 멤버의 북마크 필드에 리스트 형식으로 해당 동영상의 videoId 를 넣어주는 방식을 생각함
-        //       ------------------------------------ 임시 코드 ( 로그인 구현 완료시 수정 ) ------------------------------------
-        User user = new User(3L, "son1@gmail.com", "son12345@", "son1", "son1", "할수있다", "허리디스크", "개발자");
-        //        -------------------------------------------------------------------------------------------------------
-        List<Long> userBookmarkingList = new ArrayList<>();
-        userBookmarkingList.add(videoId);
-        user.setBookmark(userBookmarkingList);
-        userRepository.save(user);
+        User loginUser = userService.getLoginUser();
 
-        Video bookmarkedVideo = findVideo(videoId);
-        bookmarkedVideo.setBookmarkCount(bookmarkedVideo.getBookmarkCount() + 1);
-        videoRepository.save(bookmarkedVideo);
+        List<Long> userBookmarkingList = loginUser.getBookmark();
 
-        return user;
+        if (!userBookmarkingList.contains(videoId)) {
+            userBookmarkingList.add(videoId);
+            userRepository.save(loginUser);
+
+            Video bookmarkedVideo = findVideo(videoId);
+            bookmarkedVideo.setBookmarkCount(bookmarkedVideo.getBookmarkCount() + 1);
+            videoRepository.save(bookmarkedVideo);
+        }
+        return loginUser;
     }
 
+    public User removeBookmark(long videoId) {
+        User loginUser = userService.getLoginUser();
+
+        List<Long> userBookmarkingList = loginUser.getBookmark();
+
+        if (userBookmarkingList.contains(videoId)) {
+            userBookmarkingList.remove(videoId);
+            userRepository.save(loginUser);
+
+            Video bookmarkedVideo = findVideo(videoId);
+            bookmarkedVideo.setBookmarkCount(bookmarkedVideo.getBookmarkCount() - 1);
+            videoRepository.save(bookmarkedVideo);
+        }
+        return loginUser;
+    }
+
+    public String getLoginUserPrincipal() {
+        Authentication authentication = AuthUserUtils.getAuthUser();
+        Object principal = authentication.getPrincipal();
+        String email = (String) principal;
+        return email;
+    }
 }
