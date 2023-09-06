597303ecb0ab3a217b290decfec74319f44efacf
HelloWorldDogs223 <yonghyeogi377@gmail.com>
2023-09-06 10:48:51 +0900

    Main Search Page enter

--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -11,11 +11,13 @@ import {
   checkBoxListBody,
   checkBoxListJob,
 } from '../assets/constantValues';
+import { useNavigate } from 'react-router-dom';
 
 function Main() {
   const [videoType, setVideoType] = useState('전체');
   const [videoDetailType, setVideoDetailType] = useState('가슴');
   const [videoDetailType2, setVideoDetailType2] = useState('경영·사무');
+  const navigate = useNavigate();
 
   const onClickHandler = (e) => {
     setVideoType(e.target.innerText);
@@ -29,12 +31,36 @@ function Main() {
     setVideoDetailType2(e.target.innerText);
   };
 
+  const onClickSearchHandler = (e) => {
+    const content = e.target.previousSibling.value;
+    if (content === '' || content.replaceAll(' ', '').length === 0) {
+      return;
+    }
+    console.log(e.target.previousSibling.value); // 값 얻어올 수 있음
+    navigate('/search', { state: { value: content } }); // 검색 페이지로 이동
+  };
+
+  const onKeyUpHandler = (e) => {
+    const content = e.target.value;
+    if (content === '' || content.replaceAll(' ', '').length === 0) {
+      return;
+    }
+    if (e.keyCode === 13) {
+      // input enter 누를 시
+      navigate('/search', { state: { value: content } }); // 검색 페이지로 이동
+    }
+  };
+
   return (
     <MainContainer>
       <InputContainer>
-        <InputDesign placeholder='검색하기' />
+        <InputDesign onKeyUp={onKeyUpHandler} placeholder='검색하기' />
         {/* 검색하기 */}
-        <ImageDesign src='/images/magnify.png' alt='magnifier' />
+        <ImageDesign
+          onClick={onClickSearchHandler}
+          src='/images/magnify.png'
+          alt='magnifier'
+        />
       </InputContainer>
       {/* 로그인 시, 따로 맞춤추천영상이 떠야함, 리덕스 스토어에 저장된 isLoggedIn을 이용해서 조건부로 하면 될 거 같음 */}
 
--- client/src/pages/MainSearch.jsx
+++ client/src/pages/MainSearch.jsx
@@ -1,7 +1,64 @@
+import { useEffect, useState, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
+import {
+  InputContainer,
+  InputDesign,
+  ImageDesign,
+  MainContainer,
+} from '../style/Main';
+
 export default function MainSearch() {
+  const [contents, setContents] = useState([]);
+  const location = useLocation();
+
+  const inputRef = useRef(null);
+
+  useEffect(() => {
+    // axios로 전체 데이터를 받아온 후, setContents로 contents 변수 초기화
+    //setContents()
+    const SearchContent = location.state.value;
+    inputRef.current.value = SearchContent;
+  }, []);
+
+  const onClickSearchHandler = (e) => {
+    if (e.target.previousSibling === '') {
+      return;
+    }
+    // axios로 e.target.previousSibling 값을 서버로 보내서
+    // setContents(e.target.previousSibling); // 검색한 값을 불러옴
+  };
+
+  const onKeyUpHandler = (e) => {
+    if (e.target.value === '') {
+      return;
+    }
+    if (e.keyCode === 13) {
+      // setContents(e.target.value); // 마찬가지
+    }
+  };
+
   return (
-    <>
-      <div></div>
-    </>
+    <MainContainer>
+      <InputContainer>
+        <InputDesign
+          onKeyUp={onKeyUpHandler}
+          placeholder='검색하기'
+          ref={inputRef}
+        />
+        {/* 검색하기 */}
+        <ImageDesign
+          onClick={onClickSearchHandler}
+          src='/images/magnify.png'
+          alt='magnifier'
+        />
+      </InputContainer>
+      {contents ? (
+        contents.map((elem, index) => {
+          return elem;
+        })
+      ) : (
+        <></>
+      )}
+    </MainContainer>
   );
 }
