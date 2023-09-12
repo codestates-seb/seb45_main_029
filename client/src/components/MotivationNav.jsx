bedd22ff96a5274009e6733fcdd6cdc014f9efa9
Jun Seong <drawbeat0112@gmail.com>
2023-09-12 13:04:34 +0900

    포인트 페이지 구현

---
+++ client/src/components/MotivationNav.jsx
@@ -0,0 +1,58 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const links = [
+  {id: 1, text:"포인트 상점", path : "/point"},
+  {id: 2, text:"나의 다짐", path : "/motivation"},
+  {id: 3, text:"질문답변", path : "/question"},
+]
+
+const Aside = styled.aside`
+  width: 12.5rem;
+  border: 1px solid #000;
+  border-radius: 25px;
+  padding: 1.25rem;
+  font-family: var(--nanum);
+
+  ul{
+    display: flex;
+    flex-direction: column;
+  }
+  ul > li:first-child{
+    padding: 0 0 .625rem;
+  }
+  ul > li{
+    padding: 1.25rem 0 .625rem;
+    border-bottom: 1px solid #000;
+  }
+  ul > li > a{
+    font-size: 1.5625rem;
+    transition: .3s all;
+    &.active {
+      color: var(--pink);
+    }
+    &:hover{
+      color: var(--blue);
+    }
+  }
+  
+`
+
+export default function MotivationNav(){
+  return(
+    <Aside>
+      <ul className="nav">
+        {links.map((link) => {
+          const {id, text, path} = link;
+          return(
+            <li key={id}>
+            <NavLink to={path} className={({isActive}) => isActive ? "nav-link active" : "nav-link"}>
+              {text}
+            </NavLink>
+            </li>
+          )
+        })}
+      </ul>
+    </Aside>
+  )
+}
