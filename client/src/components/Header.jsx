163afea4b26ddd1b7c6ffda55e2678cb674e2aa6
Jun Seong <drawbeat0112@gmail.com>
2023-09-13 17:41:10 +0900

    헤더 수정

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,39 +1,39 @@
-
-
-import { useNavigate } from "react-router-dom";
-import {HeaderContainer,LogoContainer,HGruop,HButton } from '../style/Header.Styled';
+import { Link, NavLink } from 'react-router-dom';
+import logo from "../assets/logo.svg"
+import {HeaderContainer,HeaderSection,HdNav} from '../style/Header.Styled';
 // eslint-disable-next-line no-unused-vars
+
+const headerBtn = [
+    {id: 1, text:"동기부여" , path:"/point"},
+    {id: 2, text:"로그인" , path:"/signin"},
+    {id: 3, text:"회원가입" , path:"/signup"},
+]
 
 
 function Header () {
-    const navigate = useNavigate();
-
-    const Main = () => {
-        navigate('/');
-    }
-   
-    const boardpage = () => {
-        navigate('/boardpage');
-    }
-    const signup = () => {
-        navigate('/signup');
-    }
-    const signin = () => {
-        navigate('/signin');
-    }
     return (
         <HeaderContainer>
-        <LogoContainer onClick={Main}>
-            <div>
-                <div className="title">Rehabilitation</div>
-                <img src="/images/removebg.png" alt="LOGO" />
-            </div>
-        </LogoContainer>
-        <HGruop>
-            <HButton onClick={boardpage}>동기부여</HButton>
-            <HButton onClick={signin}>로그인</HButton>
-            <HButton onClick={signup}>회원가입</HButton>
-        </HGruop>
+            <HeaderSection className='container_wt'>
+                <h1>
+                    <Link to='/'>
+                        <img src={logo} alt="LOGO" />
+                    </Link>
+                </h1>
+                <HdNav>
+                    <ul className='hd_nav'>
+                        {headerBtn.map((list) => {
+                            const {id, text, path} = list;
+                            return(
+                                <li key={id}>
+                                    <NavLink to={path} className={({isActive}) => isActive ? "active" : ""}>
+                                        {text}
+                                    </NavLink>
+                                </li>
+                            )
+                        })}
+                    </ul>
+                </HdNav>
+            </HeaderSection>
         </HeaderContainer>
     );
 }
--- client/src/style/Header.Styled.jsx
+++ client/src/style/Header.Styled.jsx
@@ -2,71 +2,40 @@
 import  styled  from 'styled-components';
 
 export const HeaderContainer = styled.header`
-display: grid;
-grid-auto-flow: column;
-grid-template-columns: 1fr;
-width: 120rem;
-height: 4.375rem;
-justify-content: center;
-align-items: center;
-flex-shrink: 0;
-background-color:rgba(12, 33, 57, 1);
-
-`;
-
-export const LogoContainer = styled.div`
-position: relative;
-display: flex;
-justify-content: center;
-align-items: center;
-width: 4.375rem;
-height: 1.875rem;
-flex-shrink: 0;
-padding-left: 13.75rem;
-background:url("/images/removebg.png"),50% / cover no-repeat;
-
-.title {
-    position: absolute;
-    transform: translate(-50%,-50%);
-    width: 4.375rem;
-    color: #FFF;
-font-family: Nanum Pen Script;
-font-size: .875rem;
-font-style: normal;
-font-weight: 400;
-line-height: normal;
-
-}
-
- img {
-    position: absolute;
-width: 37px;
-height: 30px;
-flex-shrink: 0;
-box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25) inset;
-transform: translate(-50%,-50%);
-}
+    width: 100%;
+    height: 4.375rem;
+    position: sticky;
+    top: 0;
+    background-color: var(--navy);
 `
-export const HGruop = styled.div`
-display: flex;
-gap: 2.5rem;
-padding-right: 13.75rem;
 
+export const HeaderSection = styled.section`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 100%;
 `;
 
-export const HButton = styled.button`
-background-color:transparent; 
-text-align: center;
-font-family: Pretendard;
-font-size: .875rem;
-font-style: normal;
-font-weight: 400;
-line-height: normal;
-color:rgba(255, 255, 255, 1);
-border: none;
+export const HdNav = styled.nav`
+    .hd_nav{
+        display: flex;
+        gap: 44px;
+    }
+    .hd_nav li a{
+        display: block;
+        color: var(--white);
+        font-weight: 500;
+        font-size: 1rem;
+        transition: .3s all;
+        padding: .625rem .9375rem;
+        border-radius: 10px;
 
-&:active {
-    font-weight: bold;
-    color: rgba(97, 135, 255, 1);
-}
+        &:hover{
+            background-color: var(--pink);
+        }
+        &.active{
+            background-color: var(--pink);
+            font-weight: 600;
+        }
+    }
 `;
