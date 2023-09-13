2bceed695e42cf4f334b800a91602ccf765d0455
uudean <swj4545@naver.com>
2023-09-13 17:04:39 +0900

    Feat : 쿼리 추가

--- server/src/main/java/seb45_main_029/server/answer/repository/AnswerRepository.java
+++ server/src/main/java/seb45_main_029/server/answer/repository/AnswerRepository.java
@@ -3,9 +3,12 @@ package seb45_main_029.server.answer.repository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import seb45_main_029.server.answer.entity.Answer;
 
 public interface AnswerRepository extends JpaRepository<Answer,Long> {
 
-    Page<Answer> findByQuestionQuestionId (PageRequest pageRequest, long boardId);
+    @Query("select a from Answer a where a.isDeleted = false and a.question.questionId = :questionId")
+    Page<Answer> findByQuestionQuestionId (PageRequest pageRequest, @Param("questionId") long boardId);
 }
--- server/src/main/java/seb45_main_029/server/question/repository/QuestionRepository.java
+++ server/src/main/java/seb45_main_029/server/question/repository/QuestionRepository.java
@@ -1,8 +1,24 @@
 package seb45_main_029.server.question.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import seb45_main_029.server.question.entity.Question;
+
+import java.util.Optional;
 
 public interface QuestionRepository extends JpaRepository<Question,Long> {
 
+    @Query("SELECT q FROM Question q where q.isDeleted = false")
+    Page<Question> findAll(PageRequest pageRequest);
+
+    @Query("SELECT  q from Question q where q.isDeleted = false and q.questionId = :questionId")
+    Optional<Question> findByQuestionId(@Param("questionId") long questionId);
+
+
+    Page<Question> findByStatusIsTrue(PageRequest pageRequest);
+
+    Page<Question> findByStatusIsFalse(PageRequest pageRequest);
 }
